9256dc60fed82b4b9cb9d084df11b1098ac32c4a
aswathy2892 <aswathyasokan2892@gmail.com>
2025-04-30 12:47:39 +0400

    Models added

---
+++ HireMeNowJobPortal/Domain/Models/Interview.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models;
+
+public partial class Interview
+{
+    public Guid Id { get; set; }
+
+    public Guid? JobId { get; set; }
+
+    public Guid? Interviewee { get; set; }
+
+    public Guid? ApplicationId { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public int Status { get; set; }
+
+    public Guid? SheduledBy { get; set; }
+
+    public Guid CompanyId { get; set; }
+
+    public virtual JobApplication? Application { get; set; }
+
+    public virtual JobProviderCompany Company { get; set; } = null!;
+
+    public virtual JobSeeker? IntervieweeNavigation { get; set; }
+
+    public virtual JobPost? Job { get; set; }
+
+    public virtual CompanyUser? SheduledByNavigation { get; set; }
+}
