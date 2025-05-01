610570653b02a87e421fc261d24eefdac67761d6
aswathy2892 <aswathyasokan2892@gmail.com>
2025-05-01 10:22:50 +0400

    modified program.cs

--- HireMeNowJobPortal/HireMeNow_WebAPI/Extensions/ApplicationServiceExtensions.cs
+++ HireMeNowJobPortal/HireMeNow_WebAPI/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,14 @@
-﻿namespace HireMeNow_WebAPI.Extensions
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HireMeNow_WebAPI.Extensions
 {
-    public class ApplicationServiceExtensions1
+    public static class ApplicationServiceExtensions
     {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<NewDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
     }
 }
