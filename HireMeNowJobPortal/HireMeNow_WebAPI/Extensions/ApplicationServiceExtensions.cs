95ec81d4aeae8cc117fe0c53956c3a005dc827b7
kavijesti <kavi.inherit@gmail.com>
2025-05-01 12:05:34 +0530

    added

--- HireMeNowJobPortal/HireMeNow_WebAPI/Extensions/ApplicationServiceExtensions.cs
+++ HireMeNowJobPortal/HireMeNow_WebAPI/Extensions/ApplicationServiceExtensions.cs
@@ -1,14 +1,6 @@
-﻿using Domain.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace HireMeNow_WebAPI.Extensions
+﻿namespace HireMeNow_WebAPI.Extensions
 {
-    public static class ApplicationServiceExtensions
+    public class ApplicationServiceExtensions1
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration configuration)
-        {
-            services.AddDbContext<NewDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-            return services;
-        }
     }
 }
