3ff612c405dc34ae7c457a638c3da61ee87fd0cb
aswathy2892 <aswathyasokan2892@gmail.com>
2025-05-07 15:40:29 +0400

    added new model

---
+++ HireMeNowJobPortal/Domain/Models/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Models;
+
+public partial class Skill
+{
+    public Guid Id { get; set; } = new Guid();
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public virtual ICollection<JobSeekerProfileSkill> JobSeekerProfileSkills { get; set; }= new List<JobSeekerProfileSkill>();  
+}
+
+
+
+
